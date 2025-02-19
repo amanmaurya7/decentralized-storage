47fd725c2bcba52fd91cfde325654643b1a2b300
Aman Maurya <amanmaurya.me@gmail.com>
2025-02-19 14:39:40 +0530

    feat: Add deployment script for FileStorage contract

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+    const FileStorage = await hre.ethers.getContractFactory("FileStorage");
+    const fileStorage = await FileStorage.deploy();
+
+    await fileStorage.deployed();
+
+    console.log("FileStorage deployed to:", fileStorage.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
